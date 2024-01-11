3d28502f8480134a8a53546f0dd44313fc0d9847
ArenggaDean <135964123+MBAHTAWOONS@users.noreply.github.com>
2024-01-12 00:00:55 +0700

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -61,8 +61,8 @@ def load_model(modelzip):
 if __name__ == '__main__':
     
     modelzip = 'model.zip'
-    url = 'www.drive.google.com/uc?id=15qvS2MpqZL5Vxf3oCd-oJL2K3eH9oWZa'
-    url1 = 'https://drive.usercontent.google.com/download?id=15qvS2MpqZL5Vxf3oCd-oJL2K3eH9oWZa&authuser=0'
+    url1 = 'https://drive.google.com/uc?id=15qvS2MpqZL5Vxf3oCd-oJL2K3eH9oWZa'
+    url = 'https://drive.google.com/file/d/15qvS2MpqZL5Vxf3oCd-oJL2K3eH9oWZa/view?usp=sharing'
     
     if not os.path.exists(modelzip):
         call = st.text('Loading For Model')
